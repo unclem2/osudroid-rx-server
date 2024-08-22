fdde3022afb2c6e2438d1b73fe9dab566e0176a0
unclem2 <pavel.gutik@mail.ru>
2024-08-22 22:31:41 +0300

    od adjust

--- main.mjs
+++ main.mjs
@@ -5,7 +5,7 @@ import {
     OsuDifficultyCalculator,
     OsuPerformanceCalculator
 } from "@rian8337/osu-difficulty-calculator";
-import { readFile } from "fs";
+import { readFile } from "fs/promises";
 
 const args = process.argv.slice(2);
 if (args.length < 5) {
@@ -25,28 +25,49 @@ if (isNaN(mapid) || isNaN(accuracy_args) || isNaN(misses_args) || isNaN(combo)) 
     process.exit(1);
 }
 
-readFile(`data/beatmaps/${mapid}.osu`, { encoding: "utf-8" }, (err, data) => {
-    if (err) throw err;
+async function calculatePerformance() {
+    try {
+        const data = await readFile(`data/beatmaps/${mapid}.osu`, { encoding: "utf-8" });
+        const decoder = new BeatmapDecoder().decode(data);
 
-    const decoder = new BeatmapDecoder().decode(data);
+        const hitObjects = decoder.result.hitObjects;
+        const totalObjects = hitObjects._circles + hitObjects._sliders + hitObjects._spinners;
 
-    // console.log(decoder.result.hitObjects);
-    const accuracy = new Accuracy({
-        nmiss: misses_args,
-        nobjects: decoder.result.hitObjects._circles + decoder.result.hitObjects._sliders + decoder.result.hitObjects._spinners,
-        percent: accuracy_args,
-        
-    });
-    // console.log(accuracy);
-    const nmrating = new OsuDifficultyCalculator(decoder.result).calculate({
-        mods: mods,
-    });
+        const accuracy = new Accuracy({
+            nmiss: misses_args,
+            nobjects: totalObjects,
+            percent: accuracy_args,
+        });
 
-    const nmperformance = new OsuPerformanceCalculator(nmrating.attributes).calculate({
-        accPercent: accuracy,
-        combo: combo
-    });
-    // console.log(nmperformance);
-    const pp_return = nmperformance.total - nmperformance.speed;
-    console.log(pp_return);
-});
+        const nmrating = new OsuDifficultyCalculator(decoder.result).calculate({ mods });
+
+        const calc = {
+            speedDifficulty: nmrating.attributes.speedDifficulty,
+            mods: nmrating.attributes.mods,
+            starRating: nmrating.attributes.starRating,
+            maxCombo: nmrating.attributes.maxCombo,
+            aimDifficulty: nmrating.attributes.aimDifficulty,
+            flashlightDifficulty: nmrating.attributes.flashlightDifficulty,
+            speedNoteCount: nmrating.attributes.speedNoteCount,
+            sliderFactor: nmrating.attributes.sliderFactor,
+            approachRate: nmrating.attributes.approachRate,
+            overallDifficulty: nmrating.attributes.overallDifficulty - 4,
+            hitCircleCount: nmrating.attributes.hitCircleCount,
+            sliderCount: nmrating.attributes.sliderCount,
+            spinnerCount: nmrating.attributes.spinnerCount,
+        };
+
+        const nmperformance = new OsuPerformanceCalculator(calc).calculate({
+            accPercent: accuracy,
+            combo: combo,
+        });
+
+        const pp_return = nmperformance.total - nmperformance.speed;
+        console.log(pp_return);
+    } catch (err) {
+        console.error('Error reading or processing the beatmap file:', err);
+        process.exit(1);
+    }
+}
+
+calculatePerformance();
