62ed2f8dc535e6f549a2ee174d751ce7e2a693e9
unclem2 <pavel.gutik@mail.ru>
2024-08-22 23:28:09 +0300

    order

--- handlers/api.py
+++ handlers/api.py
@@ -71,7 +71,7 @@ async def leaderboard():
   players_stats = await glob.db.fetchall(
       'SELECT stats.id, stats.rank, stats.pp, stats.plays, users.username '
       'FROM stats '
-      'INNER JOIN users ON stats.id = users.id'
+      'INNER JOIN users ON stats.id = users.id ORDER BY stats.pp'
 )
   return jsonify(players_stats) if players_stats else {'what'}
                                    
