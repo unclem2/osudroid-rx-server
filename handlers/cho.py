7e041b2308ce5f374a3eee197ca150a6dfcb5ce6
unclem2 <pavel.gutik@mail.ru>
2024-08-28 20:51:11 +0300

    avatar lb fix

--- handlers/cho.py
+++ handlers/cho.py
@@ -148,8 +148,13 @@ async def leaderboard():
   )
   for play in plays:
     player = glob.players.get(id=int(play['playerid']))
+    
+    if os.path.isfile(f'data/avatar/{player.id}.png'):
+      avatar = f'http://{glob.config.host}:{glob.config.port}/user/avatar/{player.id}.png'
+    else:
+      avatar = f'https://s.gravatar.com/avatar/{player.email_hash}'
 
-    res += ['{play_id} {name} {score} {combo} {rank} {mods} {acc} {gravatar_hash}'.format(
+    res += ['{play_id} {name} {score} {combo} {rank} {mods} {acc} {avatar}'.format(
       play_id = play['id'],
       name = player.name,
       score = int(play['pp']) if glob.config.pp_leaderboard else play['score'],
@@ -157,7 +162,7 @@ async def leaderboard():
       rank = play['rank'],
       mods = play['mods'],
       acc = int(play['acc']*1000),
-      gravatar_hash = player.email_hash # use gravatar
+      avatar = avatar 
     )]
 
   return Success('\n'.join(res))
