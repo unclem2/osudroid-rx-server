50c5036fbb362bf1f6f8556c38a129a00b4ce978
unclem2 <pavel.gutik@mail.ru>
2024-10-05 17:05:16 +0300

    refactor 2

--- handlers/api.py
+++ handlers/api.py
@@ -1,8 +1,5 @@
-import json
-from quart import Blueprint, request, jsonify, render_template_string
-import html_templates
+from quart import Blueprint, request, jsonify
 import aiohttp
-
 from objects import glob
 from objects.beatmap import Beatmap, RankedStatus
 from objects.score import Score
@@ -30,7 +27,7 @@ async def discord_notify(msg: str):
 
 def get_player(args: list):
     if 'id' not in args and 'name' not in args:
-      return 'Need id or name', 400
+        return 'Need id or name', 400
 
     if 'id' in args:
         if not args['id'].isdecimal():
@@ -49,17 +46,17 @@ def get_player(args: list):
 
 @bp.route('/get_user')
 async def get_user():
-	args = request.args
+    args = request.args
 
-	p = get_player(args)
-	if isinstance(p, tuple):
-		return p
+    p = get_player(args)
+    if isinstance(p, tuple):
+        return p
 
-	if not p:
-		return 'Player not found', 404
+    if not p:
+        return 'Player not found', 404
 
 
-	return p.as_json
+    return p.as_json
 
 @bp.route('/get_scores')
 async def get_scores():
@@ -84,7 +81,7 @@ async def get_scores():
     if len(scores) == 0:
         return Failed('No scores found.')
     if len(scores) > 0:
-      	return jsonify(scores)
+        return jsonify(scores)
       
 
     
@@ -100,31 +97,31 @@ async def leaderboard():
       
 @bp.route('/top_scores')
 async def top_scores():
-	params = request.args
-	id = int(params.get('id'))
-	top_scores = await glob.db.fetchall(
-		'SELECT id, status, maphash, score, combo, rank, acc, "hit300", "hitgeki", '
-		'"hit100", "hitkatsu", "hit50", "hitmiss", mods, pp, date FROM scores WHERE "playerid" = $1 AND "status" = 2 AND maphash IN (SELECT md5 FROM maps WHERE status IN (1, 4, 5))'
-		'ORDER BY pp DESC',
-		[id]
-	)
-	return jsonify(top_scores) if top_scores else {'No score found.'}
+    params = request.args
+    id = int(params.get('id'))
+    top_scores = await glob.db.fetchall(
+        'SELECT id, status, maphash, score, combo, rank, acc, "hit300", "hitgeki", '
+        '"hit100", "hitkatsu", "hit50", "hitmiss", mods, pp, date FROM scores WHERE "playerid" = $1 AND "status" = 2 AND maphash IN (SELECT md5 FROM maps WHERE status IN (1, 4, 5))'
+        'ORDER BY pp DESC',
+        [id]
+    )
+    return jsonify(top_scores) if top_scores else {'No score found.'}
 
 
 #bot endpoints
 
 @bp.route('/recent')
 async def recent():
-	params = request.args
-	id = int(params.get('id'))
-	index = params.get('index')
-	recent = await glob.db.fetchall(
-		'SELECT id, status, "maphash", score, combo, rank, acc, "hit300", "hitgeki", '
-		'"hit100", "hitkatsu", "hit50", "hitmiss", mods, pp FROM scores WHERE "playerid" = $1 '
-		'ORDER BY id DESC LIMIT 1 OFFSET $2',
-		[id, index]
-	) 
-	return jsonify(recent) if recent else {'No score found.'}
+    params = request.args
+    id = int(params.get('id'))
+    index = params.get('index')
+    recent = await glob.db.fetchall(
+        'SELECT id, status, "maphash", score, combo, rank, acc, "hit300", "hitgeki", '
+        '"hit100", "hitkatsu", "hit50", "hitmiss", mods, pp FROM scores WHERE "playerid" = $1 '
+        'ORDER BY id DESC LIMIT 1 OFFSET $2',
+        [id, index]
+    )
+    return jsonify(recent) if recent else {'No score found.'}
 
 @bp.route('/calculate', methods=['POST'])
 async def calculate():
@@ -161,37 +158,37 @@ async def calculate():
 # client endpoints
 @bp.route('/update.php')
 async def send_update():
-	data = {
-		"version_code": 1727108920,
-		"link": "https://github.com/unclem2/osudroid-rx-server/releases/download/v1.13/osu.droid-1.13.240923.-debug-2024-09-23.apk",
-		"changelog": "public release"
-	}
-	return data  
+    data = {
+        "version_code": 1727108920,
+        "link": "https://github.com/unclem2/osudroid-rx-server/releases/download/v1.13/osu.droid-1.13.240923.-debug-2024-09-23.apk",
+        "changelog": "public release"
+    }
+    return data
 
 @bp.route('/v2/md5/<string:md5>')  
 async def map_status(md5:str):
   
-	map = await Beatmap.from_md5(md5)
-	if map is None:
-		return {'md5': '', 'ranked': -1}
-	await map.download()
-	
-	if map.status == RankedStatus.Whitelisted:
-		map.status = 1
-	
-	map_data = {
-		
-		"md5": md5,
-		"ranked": map.status
-	}
-	return map_data
+    map = await Beatmap.from_md5(md5)
+    if map is None:
+        return {'md5': '', 'ranked': -1}
+    await map.download()
+
+    if map.status == RankedStatus.Whitelisted:
+        map.status = 1
+
+    map_data = {
+
+        "md5": md5,
+        "ranked": map.status
+    }
+    return map_data
 
 #whitelist
 
 @bp.route('/wl')
 async def whitelist():
-	maps = await glob.db.fetchall('SELECT md5 FROM maps WHERE status = 5')
-	return jsonify(maps)
+    maps = await glob.db.fetchall('SELECT md5 FROM maps WHERE status = 5')
+    return jsonify(maps)
 
 @bp.route('/wl_add', methods=['GET'])
 async def whitelist_add():
@@ -216,9 +213,9 @@ async def whitelist_add():
   
 @bp.route('/wl_remove', methods=['GET'])
 async def whitelist_remove():
-	data = request.args
-	if data.get('md5') is not None:
-		await glob.db.execute('DELETE FROM maps WHERE md5 = $1 AND status = 5', [data.get('md5')]) 
-	if data.get('bid') is not None:
-		await glob.db.execute('DELETE FROM maps WHERE id = $1 AND status = 5', [int(data.get('bid'))])
-	return {'status': 'success'}
+    data = request.args
+    if data.get('md5') is not None:
+        await glob.db.execute('DELETE FROM maps WHERE md5 = $1 AND status = 5', [data.get('md5')])
+    if data.get('bid') is not None:
+        await glob.db.execute('DELETE FROM maps WHERE id = $1 AND status = 5', [int(data.get('bid'))])
+    return {'status': 'success'}
--- handlers/cho.py
+++ handlers/cho.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import copy
 import time
 import hashlib
 from quart import Blueprint, request, send_file, render_template_string
@@ -11,14 +10,15 @@ from objects import glob
 from objects.player import Player
 from objects.score import Score, SubmissionStatus
 from objects.beatmap import RankedStatus
-from handlers.response import Failed, Success, Failure
-
+from handlers.response import Failed, Success
 
 import utils
 import html_templates
+
 ph = PasswordHasher()
 bp = Blueprint('cho', __name__)
 bp.prefix = '/api/'
+
 
 async def discord_notify(msg: str):
     async with aiohttp.ClientSession() as session:
@@ -35,299 +35,301 @@ async def discord_notify(msg: str):
 ## Register / Login
 @bp.route('/login.php', methods=['POST'])
 async def login():
-	params = await request.form
+    params = await request.form
 
-	if 'username' not in params or len(params['username']) == 0:
-		return Failed("Invalid username.")
+    if 'username' not in params or len(params['username']) == 0:
+        return Failed("Invalid username.")
 
-	p = glob.players.get(name=params['username'])
+    p = glob.players.get(name=params['username'])
 
-	if not p:
-		return Failed("User not found.")
-	if params['version'] != '3':
-		return Failed("This client is outdated")
-	## login shites
-	res = (await glob.db.fetch("SELECT password_hash, status FROM users WHERE id = $1", [p.id])) 
-	status = res['status']
-	pswd_hash = res['password_hash']
-	hashes = glob.cache['hashes']
+    if not p:
+        return Failed("User not found.")
+    if params['version'] != '3':
+        return Failed("This client is outdated")
 
-	if pswd_hash in hashes:
-		if params['password'] != hashes[pswd_hash]:
-			return Failed('Wrong password.')
-	else:
-		try:
-			ph.verify(pswd_hash, params['password'])
-		except:
-			return Failed('Wrong password.')
+    res = (await glob.db.fetch("SELECT password_hash, status FROM users WHERE id = $1", [p.id]))
+    status = res['status']
+    pswd_hash = res['password_hash']
+    hashes = glob.cache['hashes']
 
-		hashes[pswd_hash] = params['password']
+    if pswd_hash in hashes:
+        if params['password'] != hashes[pswd_hash]:
+            return Failed('Wrong password.')
+    else:
+        try:
+            ph.verify(pswd_hash, params['password'])
+        except:
+            return Failed('Wrong password.')
 
-	if status != 0:
-		return Failed("Banned.")
+        hashes[pswd_hash] = params['password']
 
-	# update last ping
-	p.last_online = time.time()
+    if status != 0:
+        return Failed("Banned.")
 
-	# make uuid if havent
-	if not p.uuid:
-		p.uuid = utils.make_uuid(p.name)
-		
-	if os.path.isfile(f'data/avatar/{p.id}.png'):
-		p.avatar = f'http://{glob.config.host}:{glob.config.port}/user/avatar/{p.id}.png'
-	else:
-		p.avatar = f'https://s.gravatar.com/avatar/{p.email_hash}'
-	# returns long string of shit
-	return Success('{id} {uuid} {rank} {rank_by} {acc} {name} {avatar}'.format(
-		id = p.id,
-		uuid = p.uuid,
-		rank = p.stats.rank,
-		rank_by = int(p.stats.rank_by),
-		acc = p.stats.droid_acc,
-		name = p.name,
-		avatar = p.avatar
-	))
+    # update last ping
+    p.last_online = time.time()
+
+    # make uuid if havent
+    if not p.uuid:
+        p.uuid = utils.make_uuid(p.name)
+
+    if os.path.isfile(f'data/avatar/{p.id}.png'):
+        p.avatar = f'http://{glob.config.host}:{glob.config.port}/user/avatar/{p.id}.png'
+    else:
+        p.avatar = f'https://s.gravatar.com/avatar/{p.email_hash}'
+    # returns long string of shit
+    return Success('{id} {uuid} {rank} {rank_by} {acc} {name} {avatar}'.format(
+        id=p.id,
+        uuid=p.uuid,
+        rank=p.stats.rank,
+        rank_by=int(p.stats.rank_by),
+        acc=p.stats.droid_acc,
+        name=p.name,
+        avatar=p.avatar
+    ))
 
 
-@bp.route('/register.php', methods=['GET','POST'])
+@bp.route('/register.php', methods=['GET', 'POST'])
 async def register():
-	if request.method == 'POST':
-		params = await request.form
+    if request.method == 'POST':
+        params = await request.form
 
-		for args in ['username', 'password' , 'email']:
-			if not params.get(args, None):
-				return Failed('Not enough argument.')
+        for args in ['username', 'password', 'email']:
+            if not params.get(args, None):
+                return Failed('Not enough argument.')
 
-		# check username
-		if glob.players.get(name=params['username']):
-			return Failed("Username already exists.")
+        # check username
+        if glob.players.get(name=params['username']):
+            return Failed("Username already exists.")
 
-		if len(params['username']) < 2:
-			return Failed("Username must be longer than 2 characters.")
+        if len(params['username']) < 2:
+            return Failed("Username must be longer than 2 characters.")
 
-		pasw = params['password'] + "taikotaiko"
-		md5_hash = hashlib.md5()
-		md5_hash.update(pasw.encode('utf-8'))
-		pasw_hashed = md5_hash.hexdigest()
-		
-		player_id = await glob.db.execute(
-			'''
-		INSERT INTO users (
-			prefix, username, username_safe, password_hash, device_id, sign, avatar_id, custom_avatar, email, email_hash, status
-		) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id
-		''',
-		[
-			None,
-			params['username'],
-			utils.make_safe(params['username']),
-			ph.hash(pasw_hashed),
-			"okyeah",
-			'NotUsed',
-			None,
-			None,
-			params['email'],
-			utils.make_md5(params['email']),
-			0
-		]
-	)    
-		
-		# also create stats table
-		await glob.db.execute(
-			'INSERT INTO stats (id) VALUES ($1)',
-			[int(player_id)]
-		)
-		# create player
-		p = await Player.from_sql(player_id)
-		glob.players.add(p)
+        pasw = params['password'] + "taikotaiko"
+        md5_hash = hashlib.md5()
+        md5_hash.update(pasw.encode('utf-8'))
+        pasw_hashed = md5_hash.hexdigest()
 
-		return Success('Account Created.')
-	return await render_template_string(html_templates.registration_form)
+        player_id = await glob.db.execute(
+            '''
+        INSERT INTO users (
+            prefix, username, username_safe, password_hash, device_id, sign, avatar_id, custom_avatar, email, email_hash, status
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id
+        ''',
+            [
+                None,
+                params['username'],
+                utils.make_safe(params['username']),
+                ph.hash(pasw_hashed),
+                "okyeah",
+                'NotUsed',
+                None,
+                None,
+                params['email'],
+                utils.make_md5(params['email']),
+                0
+            ]
+        )
+
+        # also create stats table
+        await glob.db.execute(
+            'INSERT INTO stats (id) VALUES ($1)',
+            [int(player_id)]
+        )
+        # create player
+        p = await Player.from_sql(player_id)
+        glob.players.add(p)
+
+        return Success('Account Created.')
+    return await render_template_string(html_templates.registration_form)
+
 
 ## Leaderboard / View Replay Data / View Actual Replay / Upload Replay
 @bp.route('/getrank.php', methods=['POST'])
 async def leaderboard():
-	params = await request.form
+    params = await request.form
 
-	if 'hash' not in params:
-		return Failed('No map hash.')
+    if 'hash' not in params:
+        return Failed('No map hash.')
 
-	res = []
-	plays = await glob.db.fetchall(
-		"SELECT * FROM scores WHERE maphash = $1 AND status = 2 ORDER BY {order_by} DESC".format(
-			order_by='pp' if glob.config.pp_leaderboard else 'score'
-		),
-		[params['hash']]
-	)
-	for play in plays:
-		player = glob.players.get(id=int(play['playerid']))
-		
-		if os.path.isfile(f'data/avatar/{player.id}.png'):
-			avatar = f'http://{glob.config.host}:{glob.config.port}/user/avatar/{player.id}.png'
-		else:
-			avatar = f'https://s.gravatar.com/avatar/{player.email_hash}'
+    res = []
+    plays = await glob.db.fetchall(
+        "SELECT * FROM scores WHERE maphash = $1 AND status = 2 ORDER BY {order_by} DESC".format(
+            order_by='pp' if glob.config.pp_leaderboard else 'score'
+        ),
+        [params['hash']]
+    )
+    for play in plays:
+        player = glob.players.get(id=int(play['playerid']))
 
-		res += ['{play_id} {name} {score} {combo} {rank} {mods} {acc} {avatar}'.format(
-		play_id = play['id'],
-		name = player.name,
-		score = int(play['pp']) if glob.config.pp_leaderboard else play['score'],
-		combo = play['combo'],
-		rank = play['rank'],
-		mods = play['mods'],
-		acc = int(play['acc']*1000),
-		avatar = avatar 
-		)]
+        if os.path.isfile(f'data/avatar/{player.id}.png'):
+            avatar = f'http://{glob.config.host}:{glob.config.port}/user/avatar/{player.id}.png'
+        else:
+            avatar = f'https://s.gravatar.com/avatar/{player.email_hash}'
 
-	return Success('\n'.join(res))
+        res += ['{play_id} {name} {score} {combo} {rank} {mods} {acc} {avatar}'.format(
+            play_id=play['id'],
+            name=player.name,
+            score=int(play['pp']) if glob.config.pp_leaderboard else play['score'],
+            combo=play['combo'],
+            rank=play['rank'],
+            mods=play['mods'],
+            acc=int(play['acc'] * 1000),
+            avatar=avatar
+        )]
+
+    return Success('\n'.join(res))
+
 
 @bp.route('gettop.php', methods=['POST'])
 async def view_score():
-	params = await request.form
+    params = await request.form
 
-	play = await glob.db.fetch("SELECT * FROM scores WHERE id = $1", [int(params['playID'])])
-	if play:
-		
-		return Success('{mods} {score} {combo} {rank} {hitgeki} {hit300} {hitkatsu} {hit100} {hit50} {hitmiss} {acc} {date}'.format(
-		mods = play['mods'],
-		score = int(play['pp']) if glob.config.pp_leaderboard else play['score'],
-		combo = play['combo'],
-		rank = play['rank'],
-		hitgeki = play['hitgeki'],
-		hit300 = play['hit300'],
-		hitkatsu = play['hitkatsu'],
-		hit100 = play['hit100'],
-		hit50 = play['hit50'],
-		hitmiss = play['hitmiss'],
-		
-		acc = int(play['acc']*1000),
-		date = play['date']
-		))
+    play = await glob.db.fetch("SELECT * FROM scores WHERE id = $1", [int(params['playID'])])
+    if play:
+        return Success(
+            '{mods} {score} {combo} {rank} {hitgeki} {hit300} {hitkatsu} {hit100} {hit50} {hitmiss} {acc} {date}'.format(
+                mods=play['mods'],
+                score=int(play['pp']) if glob.config.pp_leaderboard else play['score'],
+                combo=play['combo'],
+                rank=play['rank'],
+                hitgeki=play['hitgeki'],
+                hit300=play['hit300'],
+                hitkatsu=play['hitkatsu'],
+                hit100=play['hit100'],
+                hit50=play['hit50'],
+                hitmiss=play['hitmiss'],
 
-	return Failed('Score not found.')
+                acc=int(play['acc'] * 1000),
+                date=play['date']
+            ))
+
+    return Failed('Score not found.')
+
 
 @bp.route('/upload/<string:replay_path>', methods=['GET'])
 async def view_replay(replay_path: str):
-	path = f'data/replays/{replay_path}' # already have .odr
+    path = f'data/replays/{replay_path}'  # already have .odr
 
-	if not os.path.isfile(path):
-		return Failed('Replay not found.')
+    if not os.path.isfile(path):
+        return Failed('Replay not found.')
 
-	return await send_file(path)
+    return await send_file(path)
+
 
 @bp.route('/upload.php', methods=['POST'])
 async def upload_replay():
-		# Use await with request.files and request.form for asynchronous access
-	files = await request.files
-	form = await request.form
+    # Use await with request.files and request.form for asynchronous access
+    files = await request.files
+    form = await request.form
 
-		# Correctly access the file and replayID
-	file = files.get('uploadedfile')
-	replay_id = form.get('replayID')
+    # Correctly access the file and replayID
+    file = files.get('uploadedfile')
+    replay_id = form.get('replayID')
 
-	path = f'data/replays/{replay_id}.odr' # doesnt have .odr
-	raw_replay = file.read()
-	
+    path = f'data/replays/{replay_id}.odr'  # doesnt have .odr
+    raw_replay = file.read()
 
-	if raw_replay[:2] != b'PK':
-		return Failed('Fuck off lol.')
+    if raw_replay[:2] != b'PK':
+        return Failed('Fuck off lol.')
 
-	if os.path.isfile(path):
-		return Failed('File already exists.')
+    if os.path.isfile(path):
+        return Failed('File already exists.')
 
-	with open(path, 'wb') as file:
-		file.write(raw_replay)
+    with open(path, 'wb') as file:
+        file.write(raw_replay)
 
-	return Success('Replay uploaded.')
+    return Success('Replay uploaded.')
+
 
 ## Play Submit - god i hate this part
 
 @bp.route('/submit.php', methods=['POST'])
 async def submit_play():
-	params = await request.form
+    params = await request.form
 
-	if 'userID' not in params:
-		return Failed('Not enough argument.')
+    if 'userID' not in params:
+        return Failed('Not enough argument.')
 
-	p = glob.players.get(id=int(params['userID']))
-	p.last_online = time.time()
+    p = glob.players.get(id=int(params['userID']))
+    p.last_online = time.time()
 
-	if not p:
-		return Failed('Player not found, report to server admin.')
+    if not p:
+        return Failed('Player not found, report to server admin.')
 
-	if 'ssid' in params:
-		if params['ssid'] != p.uuid:
-			return Failed('Server restart, please relogin.')
+    if 'ssid' in params:
+        if params['ssid'] != p.uuid:
+            return Failed('Server restart, please relogin.')
 
-	if glob.config.disable_submit:
-		return Failed('Score submission is disable right now.')
+    if glob.config.disable_submit:
+        return Failed('Score submission is disable right now.')
 
-	if (map_hash := params.get('hash', None)):
-		logging.info(f'Changed {p} playing to {map_hash}')
-		p.stats.playing = map_hash
-		return Success(1, p.id)
+    if (map_hash := params.get('hash', None)):
+        logging.info(f'Changed {p} playing to {map_hash}')
+        p.stats.playing = map_hash
+        return Success(1, p.id)
 
-	elif (play_data := params.get('data')):
-		s = await Score.from_submission(play_data)
+    elif (play_data := params.get('data')):
+        s = await Score.from_submission(play_data)
 
-		if not s:
-			return Failed('Failed to read score data.')
+        if not s:
+            return Failed('Failed to read score data.')
 
-		if s.status == SubmissionStatus.BEST:
-			# if this score is better change old play status to 1
-			await glob.db.execute('UPDATE scores SET status = 1 WHERE status = 2 AND mapHash = $1 AND playerID = $2', [s.map_hash, s.player.id])
-		if s.map_hash == None:
-			return Failed('Server cannot find your recent play, maybe it restarted?')
-		elif not s.player:
-			return Failed('Player not found, report to server admin.')
-		elif not s.bmap:
-			# Map does not exists, most likely its a shit map.
-			# Returns current stats
-			return Success(s.player.stats.droid_submit_stats)
-		elif s.bmap.status == RankedStatus.Pending:
-			# User can remove this themselves, I'm just following gulag's osuSubmitModularSelector.
-			return Success(s.player.stats.droid_submit_stats)
+        if s.status == SubmissionStatus.BEST:
+            # if this score is better change old play status to 1
+            await glob.db.execute('UPDATE scores SET status = 1 WHERE status = 2 AND mapHash = $1 AND playerID = $2',
+                                  [s.map_hash, s.player.id])
+        if s.map_hash == None:
+            return Failed('Server cannot find your recent play, maybe it restarted?')
+        elif not s.player:
+            return Failed('Player not found, report to server admin.')
+        elif not s.bmap:
+            # Map does not exists, most likely its a shit map.
+            # Returns current stats
+            return Success(s.player.stats.droid_submit_stats)
+        elif s.bmap.status == RankedStatus.Pending:
+            # User can remove this themselves, I'm just following gulag's osuSubmitModularSelector.
+            return Success(s.player.stats.droid_submit_stats)
 
-		vals = [s.status, s.map_hash, s.player.id, s.score, s.max_combo, s.grade, s.acc, s.h300, s.hgeki, s.h100, s.hkatsu, s.h50, s.hmiss, s.mods, s.pp, s.bmap.id, s.date]
-		s.id = await glob.db.execute('''
-			INSERT INTO scores (status, mapHash, playerID, score, combo, rank, acc, hit300, hitgeki, hit100, hitkatsu, hit50, hitmiss, mods, pp, mapid, date)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
-		''', vals)
-		upload_replay = False
-		if s.status == SubmissionStatus.BEST:
-			upload_replay = True
+        vals = [s.status, s.map_hash, s.player.id, s.score, s.max_combo, s.grade, s.acc, s.h300, s.hgeki, s.h100,
+                s.hkatsu, s.h50, s.hmiss, s.mods, s.pp, s.bmap.id, s.date]
+        s.id = await glob.db.execute('''
+            INSERT INTO scores (status, mapHash, playerID, score, combo, rank, acc, hit300, hitgeki, hit100, hitkatsu, hit50, hitmiss, mods, pp, mapid, date)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+        ''', vals)
+        upload_replay = False
+        if s.status == SubmissionStatus.BEST:
+            upload_replay = True
 
-		## Update stats
-		stats = s.player.stats
+        ## Update stats
+        stats = s.player.stats
 
-		stats.plays += 1
-		stats.tscore = s.score
+        stats.plays += 1
+        stats.tscore = s.score
 
-		if s.status == SubmissionStatus.BEST and s.bmap.gives_reward:
-			additive = s.score
+        if s.status == SubmissionStatus.BEST and s.bmap.gives_reward:
+            additive = s.score
 
-			if s.prev_best:
-				additive -= s.prev_best.score
+            if s.prev_best:
+                additive -= s.prev_best.score
 
-			stats.rscore += additive
-		
+            stats.rscore += additive
 
-		## Update Ranked Score stats to db
-		await glob.db.execute(
-		'UPDATE stats SET rscore = $1, tscore = $2, plays = $3 WHERE id = $4',
-		[stats.rscore, stats.tscore, stats.plays, s.player.id])
+        ## Update Ranked Score stats to db
+        await glob.db.execute(
+            'UPDATE stats SET rscore = $1, tscore = $2, plays = $3 WHERE id = $4',
+            [stats.rscore, stats.tscore, stats.plays, s.player.id])
 
-		## Update stats one more time - i know this is retarded cuz we're already doing it above but im basing it from the old code so
-		await s.player.update_stats()
-		
-		await discord_notify(f'{s.player.name} has submitted a play on {s.bmap.full} with {s.pp}pp!')
-		return Success('{rank} {rank_by} {acc} {map_rank} {score_id}'.format(
-		rank = int(stats.rank),
-		rank_by = int(stats.rank_by),
-		acc = stats.droid_acc,
-		map_rank = s.rank,
-		score_id = s.id if upload_replay else ""
-		))
+        ## Update stats one more time - i know this is retarded cuz we're already doing it above but im basing it from the old code so
+        await s.player.update_stats()
 
-	return Failed('Huh?') # Client wasnt supposed to get here.
+        await discord_notify(f'{s.player.name} has submitted a play on {s.bmap.full} with {s.pp}pp!')
+        return Success('{rank} {rank_by} {acc} {map_rank} {score_id}'.format(
+            rank=int(stats.rank),
+            rank_by=int(stats.rank_by),
+            acc=stats.droid_acc,
+            map_rank=s.rank,
+            score_id=s.id if upload_replay else ""
+        ))
 
-
-
+    return Failed('Huh?')  # Client wasnt supposed to get here.
