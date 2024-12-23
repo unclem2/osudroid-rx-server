e40114d97ffb3e6364f811756d5e14b0209ccc8e
unclem2 <pavel.gutik@mail.ru>
2024-12-23 23:07:09 +0300

    type error on pp

--- utils/pp.py
+++ utils/pp.py
@@ -158,7 +158,7 @@ class PPCalculator:
         aim_pp = aim_pp - aim_pp * stream_percentage
         pp_return = attributes.pp - attributes.pp_speed - attributes.pp_aim + aim_pp
         pp_return = pp_return * acc_factor * speed_reduction_factor * force_ar_penalty
-        if pp_return >= glob.config.max_pp_value:
+        if float(pp_return) >= float(glob.config.max_pp_value):
             return 0
         return pp_return 
 
