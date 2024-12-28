6b0ff7e6dc286b643394e68663726af3d83cb228
unclem2 <pavel.gutik@mail.ru>
2024-12-28 04:26:16 +0300

    changelog

--- handlers/api.py
+++ handlers/api.py
@@ -150,7 +150,7 @@ async def send_update():
     data = {
         "version_code": 1735348672,
         "link": "https://github.com/unclem2/osudroid-rx-server/releases/download/v1.13.1/osu.droid-1.13.241228.-debug-2024-12-28.apk",
-        "changelog": "https"
+        "changelog": "new domain"
     }
     return data
 
