c97a7ceea4284bd9d6733b35a162c00f09592d39
unclem2 <pavel.gutik@mail.ru>
2024-12-29 00:59:17 +0300

    http -> https

--- handlers/cho.py
+++ handlers/cho.py
@@ -153,7 +153,7 @@ async def leaderboard():
         player = glob.players.get(id=int(play['playerid']))
 
         if os.path.isfile(f'data/avatar/{player.id}.png'):
-            avatar = f'http://{glob.config.host}:{glob.config.port}/user/avatar/{player.id}.png'
+            avatar = f'https://{glob.config.host}:{glob.config.port}/user/avatar/{player.id}.png'
         else:
             avatar = f'https://s.gravatar.com/avatar/{player.email_hash}'
 
