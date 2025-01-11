6fbaa4d8b82c5e24f8feb8619f8bb4be143bf456
unclem2 <pavel.gutik@mail.ru>
2025-01-11 21:30:02 +0300

    get_scores fix

--- handlers/api/get_scores.py
+++ handlers/api/get_scores.py
@@ -11,17 +11,18 @@ async def get_scores():
     params = request.args
 
     limit = int(params.get("limit", 50)) if utils.is_convertable(params.get("limit", 50), int) else 50
+    
     if utils.is_convertable(params.get("id", 0), int):
         id = int(params.get("id", 0))
     else:
         return {"error": "Invalid id."}, 400
 
-    scores = await glob.db.fetchall(
-        'SELECT id, status, "maphash", score, combo, rank, acc, "hit300", "hitgeki", '
-        '"hit100", "hitkatsu", "hit50", "hitmiss", mods, pp, date FROM scores WHERE "playerid" = $1 '
-        "ORDER BY id DESC LIMIT $2",
-        [id, limit],
-    )
+    query = 'SELECT id, status, "maphash", score, combo, rank, acc, "hit300", "hitgeki", ' \
+            '"hit100", "hitkatsu", "hit50", "hitmiss", mods, pp, date FROM scores WHERE "playerid" = $1 ' \
+            "ORDER BY id DESC"
+    if limit != -1:
+        query += f" LIMIT {limit}"      
+    scores = await glob.db.fetchall(query, [id])
 
     for score in scores:
         try:
