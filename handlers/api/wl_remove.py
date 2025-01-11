9bd502db946ea673ff420d9682a62fd690b5b47b
unclem2 <74730671+unclem2@users.noreply.github.com>
2025-01-12 02:52:16 +0300

    Update handlers/api/wl_remove.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- handlers/api/wl_remove.py
+++ handlers/api/wl_remove.py
@@ -10,7 +10,6 @@ async def whitelist_remove():
     data = request.args
     if data.get("key") != os.getenv("WL_KEY"):
         return {"status": "error", "message": "Key not specified or incorrect."}
-    
     if data.get("md5") is not None:
         await glob.db.execute(
             "UPDATE maps SET status = -2 WHERE md5 = $1", [data.get("md5")]
