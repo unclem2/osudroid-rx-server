fe781d7fba76009ec7481f7ecd0fc96cbb61b9d2
unclem2 <74730671+unclem2@users.noreply.github.com>
2025-01-21 15:26:02 +0000

    :art: Format Python code with psf/black

--- handlers/user/profile.py
+++ handlers/user/profile.py
@@ -48,7 +48,7 @@ async def profile():
                 "%Y-%m-%d %H:%M:%S", time.gmtime(score["date"] / 1000)
             )
             score["acc"] = f"{score['acc']:.2f}%"
-            
+
             score["pp"] = f"{round(score['pp'])}pp"
 
             score["mods"] = f"{Mods(score['mods']).convert_std}"
