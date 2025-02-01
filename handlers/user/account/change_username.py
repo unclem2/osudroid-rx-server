8dc4dfcfb84751cba7517a5fc68ac358f30f97c8
75efb6 <71222525+75efb6@users.noreply.github.com>
2025-02-01 01:04:59 +0000

    :art: Format Python code with psf/black

--- handlers/user/account/change_username.py
+++ handlers/user/account/change_username.py
@@ -6,6 +6,7 @@ import utils
 
 bp = Blueprint("user_change_username", __name__)
 
+
 @bp.route("/", methods=["POST"])
 async def change_username():
     login_state = request.cookies.get("login_state")
@@ -19,23 +20,30 @@ async def change_username():
             utils.check_md5(f"{username}-{player_id}-{os.getenv('KEY')}", auth_hash)
             == False
         ):
-            return await render_template("error.jinja", error_message="Invalid login state")
+            return await render_template(
+                "error.jinja", error_message="Invalid login state"
+            )
         new_username = req.get("new_username")
         if not new_username:
-            return await render_template("error.jinja", error_message="Invalid new username")
+            return await render_template(
+                "error.jinja", error_message="Invalid new username"
+            )
 
         player = glob.players.get(id=int(player_id))
         if not player or player.id != int(player_id):
-            return await render_template("error.jinja", error_message="Player not found")
+            return await render_template(
+                "error.jinja", error_message="Player not found"
+            )
 
-        res = await glob.db.fetch(
-            "SELECT status FROM users WHERE id = $1", [player.id]
-        )
+        res = await glob.db.fetch("SELECT status FROM users WHERE id = $1", [player.id])
         if not res:
-            return await render_template("error.jinja", error_message="Player not found")
+            return await render_template(
+                "error.jinja", error_message="Player not found"
+            )
         safe_username = utils.make_safe(new_username)
         await glob.db.execute(
-            "UPDATE users SET username = $1, username_safe = $2 WHERE id = $3", [new_username, safe_username, player.id]
+            "UPDATE users SET username = $1, username_safe = $2 WHERE id = $3",
+            [new_username, safe_username, player.id],
         )
         return await render_template(
             "success.jinja", success_message="Username changed successfully"
