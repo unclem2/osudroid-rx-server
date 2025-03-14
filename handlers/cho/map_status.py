8151889fef58c586d4857f5c2ebabc3694c6b30b
unclem2 <pavel.gutik@mail.ru>
2025-03-15 02:10:06 +0300

    fixes

--- handlers/cho/map_status.py
+++ handlers/cho/map_status.py
@@ -9,8 +9,9 @@ forced_route = "/api/v2/md5/<string:md5>"
 
 @bp.route("/")
 async def map_status(md5: str):
-    map = await Beatmap.from_md5(md5)
-    if map is None:
+    try:
+        map = await Beatmap.from_md5(md5)
+    except:
         return {"md5": "", "ranked": -1}
     await map.download()
 
