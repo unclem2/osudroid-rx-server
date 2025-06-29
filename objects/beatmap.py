9bdc4cf0bee1343b2d500abe72611121bbdc014f
unclem2 <--global>
2025-06-29 12:52:45 +0300

    Enhance Beatmap and RankedStatus classes with detailed docstrings and type hints for better clarity and maintainability

--- objects/beatmap.py
+++ objects/beatmap.py
@@ -5,12 +5,26 @@ from datetime import datetime
 from pathlib import Path
 import json
 from objects import glob
+from typing import Optional
 
 beatmap_folder = Path.cwd() / "data/beatmaps"
 
 
 @unique
 class RankedStatus(IntEnum):
+    """
+    Enum representing the ranked status of a beatmap.
+
+    Attributes:
+        Graveyard (int): -2 — The beatmap is in the graveyard (locally or on Bancho).
+        NotSubmitted (int): -1 — The beatmap is not found (locally or on Bancho).
+        Pending (int): 0 — The beatmap is pending (Bancho).
+        Ranked (int): 1 — The beatmap is ranked (Bancho).
+        Approved (int): 2 — The beatmap is approved (Bancho).
+        Qualified (int): 3 — The beatmap is qualified (Bancho).
+        Loved (int): 4 — The beatmap is loved (Bancho).
+        Whitelisted (int): 5 — The beatmap is whitelisted (locally).
+    """
     Graveyard = -2
     NotSubmitted = -1
     Pending = 0
@@ -25,10 +39,33 @@ class RankedStatus(IntEnum):
 
 
 class Beatmap:
+    """
+    Represents a beatmap object with various attributes and methods to interact with it.
+    Attributes:
+        id (int): Beatmap ID from bancho.
+        set_id (int): Beatmapset ID from bancho.
+        md5 (str): MD5 hash of the beatmap file.
+        artist (str): Artist of the beatmap.
+        title (str): Title of the beatmap.
+        version (str): Version of the beatmap.
+        creator (str): Creator of the beatmap.
+        last_update (datetime): Last update time of the beatmap.
+        total_length (float): Total length of the beatmap in seconds.
+        max_combo (int): Maximum combo of the beatmap.
+        status (RankedStatus): Map ranked status, represented by the RankedStatus enum.
+        mode (int): Game mode of the beatmap.(not really used)
+        bpm (float): BPM of the beatmap.
+        cs (float): Circle size of the beatmap.
+        od (float): Overall difficulty of the beatmap.
+        ar (float): Approach rate of the beatmap.
+        hp (float): Health drain of the beatmap.
+        star (float): Star rating of the beatmap.
+    """
     def __init__(self, **kwargs):
-        self.id: int = kwargs.get("id", -1)
-        self.set_id: int = kwargs.get("set_id", -1)
-        self.md5: str = kwargs.get("md5", "")
+
+        self.id:int = int(kwargs["id"] if "id" in kwargs else kwargs.get("beatmap_id", -1))
+        self.set_id: int = int(kwargs["set_id"] if "set_id" in kwargs else kwargs.get("beatmapset_id", -1))
+        self.md5: str = kwargs.get("md5", "") or kwargs.get("file_md5", "")
 
         self.artist: str = kwargs.get("artist", "")
         self.title: str = kwargs.get("title", "")
@@ -36,30 +73,33 @@ class Beatmap:
         self.creator: str = kwargs.get("creator", "")
 
         self.last_update: datetime = kwargs.get("last_update", 0.0)
-        self.total_length: float = kwargs.get("total_length", 0.0)
-        self.max_combo: int = kwargs.get("max_combo", 0)
+        self.total_length: float = float(kwargs.get("total_length", 0.0))
+        self.max_combo: int = int(kwargs.get("max_combo", 0))
 
-        self.status: RankedStatus = RankedStatus(kwargs.get("status", -1))
+        self.status: RankedStatus = RankedStatus(int(kwargs["status"] if "status" in kwargs else kwargs.get("approved", -1)))
 
-        self.mode: int = kwargs.get("mode", 0)
-        self.bpm: float = kwargs.get("bpm", 0.0)
-        self.cs: float = kwargs.get("cs", 0.0)
-        self.od: float = kwargs.get("od", 0.0)
-        self.ar: float = kwargs.get("ar", 0.0)
-        self.hp: float = kwargs.get("hp", 0.0)
+        self.mode: int = int(kwargs.get("mode", 0))
+        self.bpm: float = float(kwargs.get("bpm", 0.0))
+        self.cs: float = float(kwargs.get("cs", 0.0) or kwargs.get("diff_size", 0.0))
+        self.od: float = float(kwargs.get("od", 0.0) or kwargs.get("diff_overall", 0.0))
+        self.ar: float = float(kwargs.get("ar", 0.0) or kwargs.get("diff_approach", 0.0))
+        self.hp: float = float(kwargs.get("hp", 0.0) or kwargs.get("diff_drain", 0.0))
 
-        self.star: float = kwargs.get("star", 0)
+        self.star: float = float(kwargs.get("star", 0))
 
     @property
-    def filename(self):
+    def filename(self) -> str:
+        """Returns the filename of the beatmap in the format: ID.osu"""
         return f"{self.id}.osu"
 
     @property
-    def full(self):
+    def full(self) -> str:
+        """Returns the full name of the beatmap in the format: Artist - Title [Version]"""
         return f"{self.artist} - {self.title} [{self.version}]"
 
     @property
-    def gives_reward(self):
+    def gives_reward(self) -> bool:
+        """Returns True if the beatmap is ranked, approved, loved, or whitelisted"""
         return self.status in (
             RankedStatus.Ranked,
             RankedStatus.Approved,
@@ -67,12 +107,18 @@ class Beatmap:
             RankedStatus.Whitelisted,
         )
 
-    async def download(self):
-        """Download the map and returns the path"""
+    async def download(self) -> Optional[Path]:
+        """
+        Download the map using old.ppy.sh/osu/{id} and save it to the beatmap folder.
+        Returns:
+            Optional[Path]: The path to the downloaded beatmap file if successful, otherwise None.
+        """
+        # Check if the beatmap is already downloaded
         path = beatmap_folder / self.filename
         if path.exists():
             return path
 
+        # Download the .osu file from bancho
         url = f"https://old.ppy.sh/osu/{self.id}"
         async with aiohttp.ClientSession() as sess:
             async with sess.get(url) as res:
@@ -81,37 +127,57 @@ class Beatmap:
 
                 content = await res.read()
 
+        # Write the content to path
         path.write_bytes(content)
         return path
 
     @classmethod
-    async def from_md5(cls, md5: str):
-        if bmap := glob.cache["beatmaps"].get(md5):
-            return bmap  # maybe add timeout like gulag?
+    async def from_md5(cls, md5: str) -> Optional["Beatmap"]:
+        """
+        Fetch a beatmap by its MD5 hash.
+        Args:
+            md5 (str): The MD5 hash of the beatmap.
+        Returns:
+            Optional[Beatmap]: The beatmap object if found, otherwise None.
+            """
+        
+        # Return cached beatmap if it exists
+        if beatmap := glob.cache["beatmaps"].get(md5):
+            return beatmap  
 
+        # If the beatmap is unsubmitted, return None
         if md5 in glob.cache["unsubmitted"]:
             return
+        
+        # Try to get beatmap from database
         try:
-            bmap = await cls.from_md5_sql(md5)
+            beatmap = await cls.from_md5_sql(md5)
         except:
-            bmap = None
-        if bmap is None:
-            # Not found in md5, try get from osu!api
+            beatmap = None
 
-            # check api key beforehand
+        # If not found in database, try to get it from osuapi
+        if beatmap is None:
             if len(glob.config.osu_key) < 32:
                 logging.info("Failed to get beatmap, invalid api key.")
                 return
 
-            if not (bmap := await cls.from_md5_osuapi(md5)):
+            if not (beatmap := await cls.from_md5_osuapi(md5)):
                 return
 
-        # saves to cache
-        glob.cache["beatmaps"][md5] = bmap
-        return bmap
+        # Put the beatmap in the cache
+        glob.cache["beatmaps"][md5] = beatmap
+        return beatmap
 
     @classmethod
-    async def from_md5_sql(cls, md5: str):
+    async def from_md5_sql(cls, md5: str) -> Optional["Beatmap"]:
+        """
+        Fetch a beatmap by its MD5 hash from the database.
+        Args:
+            md5 (str): The MD5 hash of the beatmap.
+        Returns:
+            Optional[Beatmap]: The beatmap object if found, otherwise None.
+        """
+        
         if res := await glob.db.fetch(
             "SELECT id, set_id, "
             "artist, title, version, creator, "
@@ -124,99 +190,86 @@ class Beatmap:
         ):
             return cls(**res)
 
+
     @classmethod
-    async def from_md5_osuapi(cls, md5: str):
+    async def from_md5_osuapi(cls, md5: str) -> Optional["Beatmap"]:
+        """
+        Fetches a beatmap from the old osu! API using its MD5 hash and constructs a Beatmap instance.
+
+        Args:
+            md5 (str): The MD5 hash of the beatmap file.
+
+        Returns:
+            Optional[Beatmap]: The created Beatmap object if successful; None otherwise.
+        """
         url = "https://old.ppy.sh/api/get_beatmaps"
         params = {"k": glob.config.osu_key, "h": md5}
 
-        async with aiohttp.ClientSession() as sess:
-            async with sess.get(url, params=params) as res:
-                if not res or res.status != 200:
-                    return  # fucked up big time
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as response:
+                if response.status != 200:
+                    return None
 
-                if not (data := await res.json()):
-                    return
+                data = await response.json()
+                if not data:
+                    return None 
 
-                bmap = data[0]
+        bmap = data[0]
 
-        m = cls()
-        m.md5 = md5
-        m.id = int(bmap["beatmap_id"])
-        m.set_id = int(bmap["beatmapset_id"])
+        # Create a Beatmap instance using API response
+        beatmap = cls(**bmap)
+        beatmap.md5 = md5
+        beatmap.last_update = datetime.strptime(bmap["last_update"], "%Y-%m-%d %H:%M:%S")
 
-        m.artist, m.title, m.version, m.creator = (
-            bmap["artist"],
-            bmap["title"],
-            bmap["version"],
-            bmap["creator"],
-        )
-        m.last_update = datetime.strptime(bmap["last_update"], "%Y-%m-%d %H:%M:%S")
-        m.total_length = float(bmap["total_length"])
-        m.max_combo = float(bmap["max_combo"])
+        # Optionally save to local SQL database
+        await beatmap.save_to_sql()
 
-        m.status = RankedStatus(int(bmap["approved"]))
-        m.mode = int(bmap["mode"])
-        m.bpm = float(bmap["bpm"])
+        return beatmap
 
-        m.cs, m.od, m.ar, m.hp = (
-            float(bmap["diff_size"]),
-            float(bmap["diff_overall"]),
-            float(bmap["diff_approach"]),
-            float(bmap["diff_drain"]),
-        )
-
-        m.star = float(bmap["difficultyrating"])
-        await m.save_to_sql()
-
-        return m
 
     @classmethod
-    async def from_bid_osuapi(cls, bid):
+    async def from_bid_osuapi(cls, bid: int) -> Optional["Beatmap"]:
+        """
+        Fetches a beatmap from the old osu! API using its beatmap ID (bid) and constructs a Beatmap instance.
+
+        Args:
+            bid (int): The beatmap ID from osu!.
+
+        Returns:
+            Optional[Beatmap]: The created Beatmap object if successful; None otherwise.
+        """
         url = "https://old.ppy.sh/api/get_beatmaps"
         params = {"k": glob.config.osu_key, "b": bid}
-        async with aiohttp.ClientSession() as sess:
-            async with sess.get(url, params=params) as res:
-                if not res or res.status != 200:
-                    return  # fucked up big time
 
-                if not (data := await res.json()):
-                    return
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as response:
+                if response.status != 200:
+                    return None
 
-                bmap = data[0]
+                data = await response.json()
+                if not data:
+                    return None
 
-        m = cls()
-        m.md5 = bmap["file_md5"]
-        m.id = int(bmap["beatmap_id"])
-        m.set_id = int(bmap["beatmapset_id"])
+        bmap = data[0]
 
-        m.artist, m.title, m.version, m.creator = (
-            bmap["artist"],
-            bmap["title"],
-            bmap["version"],
-            bmap["creator"],
-        )
-        m.last_update = datetime.strptime(bmap["last_update"], "%Y-%m-%d %H:%M:%S")
-        m.total_length = float(bmap["total_length"])
-        m.max_combo = float(bmap["max_combo"])
+        # Create a Beatmap instance using API response
+        beatmap = cls(**bmap)
+        beatmap.last_update = datetime.strptime(bmap["last_update"], "%Y-%m-%d %H:%M:%S")
 
-        m.status = RankedStatus(int(bmap["approved"]))
-        m.mode = int(bmap["mode"])
-        m.bpm = float(bmap["bpm"])
+        # Optionally save to database
+        await beatmap.save_to_sql()
 
-        m.cs, m.od, m.ar, m.hp = (
-            float(bmap["diff_size"]),
-            float(bmap["diff_overall"]),
-            float(bmap["diff_approach"]),
-            float(bmap["diff_drain"]),
-        )
+        return beatmap
 
-        m.star = float(bmap["difficultyrating"])
-        await m.save_to_sql()
-
-        return m
 
     @property
-    def as_json(self):
+    def as_json(self) -> dict:
+        """
+        Returns a dictionary representation of the beatmap object.
+        This is useful for serialization or API responses.
+        Returns:
+            dict: A dictionary containing the beatmap's attributes.
+        """
         return {
             "id": self.id,
             "set_id": self.set_id,
@@ -238,7 +291,12 @@ class Beatmap:
             "star": self.star,
         }
 
-    async def save_to_sql(self):
+    async def save_to_sql(self) -> None:
+        """        
+        Saves the beatmap object to the SQL database.
+        If the beatmap already exists, it updates the existing record.
+        """
+
         # Convert datetime objects to Unix timestamp integers
         last_update_int = (
             int(self.last_update.timestamp())
