67e298976a08f89949b24410d67b7aeb1dd269a0
75efb6 <71222525+75efb6@users.noreply.github.com>
2025-08-24 00:20:56 +0200

    Add default avatar when avatar does not exist.

--- handlers/user/avatar.py
+++ handlers/user/avatar.py
@@ -9,5 +9,7 @@ forced_route = "/user/avatar/<int:uid>.png"
 @bp.route("/")
 async def avatar(uid: int):
     user_avatar = pathlib.Path(f"./data/avatar/{uid}.png")
+    if not user_avatar.exists():
+        user_avatar = pathlib.Path("./data/avatar/default.png")
 
     return await send_file(user_avatar, mimetype="image/png")
