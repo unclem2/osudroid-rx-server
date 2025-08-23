9c2112e96473d1f37401359f58a01dd2b51517be
unclem2 <--global>
2025-08-23 21:31:15 +0300

    get submitted

--- handlers/multi/room_history.py
+++ handlers/multi/room_history.py
@@ -9,24 +9,10 @@ bp = Blueprint("room_history", __name__)
 async def room_history():
     data = request.args
     room_log = read_room_log(data.get("id"))
-    out_type = data.get("type")
     new_data = []
+    ret = []
+    for record in room_log:
+        if record["event"] == "allPlayersScoreSubmitted":
+            ret.append(record)
 
-    if out_type == "match":
-        room_match = {}
-        for i in room_log:
-            if (
-                i["event"] == "beatmapChanged"
-                and i["direction"] == "out"
-                and i["data"] != {}
-            ):
-                room_match["md5"] = i["data"]["md5"]
-                room_match["map_name"] = (
-                    f"{i['data']['artist']} - {i['data']['title']} ({i['data']['creator']}) [{i['data']['version']}]"
-                )
-            elif i["event"] == "allPlayersScoreSubmitted":
-                room_match["scores"] = i["data"]
-                new_data.append(room_match)
-                room_match = {}
-
-    return jsonify(new_data)
+    return jsonify(ret)
