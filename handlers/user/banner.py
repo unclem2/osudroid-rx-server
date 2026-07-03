536ae21f1c1ef0428908e1321fb6f6e1e6863ea4
unclem2 <74730671+unclem2@users.noreply.github.com>
2026-07-03 10:05:44 +0300

    fix paths

--- handlers/user/banner.py
+++ handlers/user/banner.py
@@ -8,8 +8,8 @@ forced_route = "/user/banner/<int:uid>.png"
 
 @bp.route("/")
 async def banner(uid: int):
-    user_banner = pathlib.Path(f".//srv/odrx_storage/banner/{uid}.png")
+    user_banner = pathlib.Path(f"/srv/odrx_storage/banner/{uid}.png")
     if not user_banner.exists():
-        user_banner = pathlib.Path(".//srv/odrx_storage/banner/default.png")
+        user_banner = pathlib.Path("/srv/odrx_storage/banner/default.png")
 
     return await send_file(user_banner, mimetype="image/png")
